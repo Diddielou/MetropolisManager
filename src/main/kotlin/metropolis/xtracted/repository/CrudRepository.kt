d7c24910db339bf463701e27a6998d956314328b
Dinah Bolli <diddielou@gmail.com>
2022-12-15 08:58:16 +0100

    Add xtracted files

---
+++ src/main/kotlin/metropolis/xtracted/repository/CrudRepository.kt
@@ -0,0 +1,46 @@
+package metropolis.xtracted.repository
+
+import java.sql.ResultSet
+import metropolis.xtracted.data.DbColumn
+
+class CrudRepository<D: Identifiable>(private val url        : String,
+                                      private val table      : String,
+                                      private val idColumn   : DbColumn,
+                                      private val dataColumns: Map<DbColumn, (D) -> String?>, //maps DbColumn to cell value of data
+                                      private val mapper     : ResultSet.() -> D) {
+
+    fun createKey() : Int =
+        insertAndCreateKey(url        = url,
+                           insertStmt = """INSERT INTO $table DEFAULT VALUES """.trimMargin())
+
+    fun read(id: Int) : D?  =
+        readFirst(url     = url,
+                  table   = table,
+                  columns = "$idColumn, " + dataColumns.keys.joinToString(),
+                  where   = "$idColumn = $id",
+                  map     = { mapper() })
+
+    fun update(data: D){
+        val valueUpdates = StringBuilder()
+        dataColumns.entries.forEachIndexed { index, entry ->
+            valueUpdates.append(entry.key.name)
+            valueUpdates.append(" = ")
+            valueUpdates.append(entry.value(data))
+            if(index < dataColumns.size - 1){
+                valueUpdates.append(", ")
+            }
+
+        }
+        update(url          = url,
+               table        = table,
+               id           = data.id,
+               setStatement = """SET $valueUpdates """)
+
+    }
+
+    fun delete(id: Int) =
+        delete(url   = url,
+               table = table,
+               id    = id)
+
+}
